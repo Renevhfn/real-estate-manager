6f7db368122d42e8ac261dbd041261c81aa0eed1
Renevhfn <rene.vhfn@gmail.com>
2025-03-21 14:54:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# real-estate-manager
+import streamlit as st
+from setup_database import create_database
+
+st.title('🏡 Real Estate Manager')
+
+# Button to create the database
+if st.button("Setup Database"):
+    create_database()
+    st.success("Database created successfully!")
+
